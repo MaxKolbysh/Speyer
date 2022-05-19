922d887dc4f82f3279016330914468a6107969cf
Max <int1123875@hotmail.com>
2022-05-19 17:57:55 +0200

    initial

---
+++ assets/js/elementinterface.js
@@ -0,0 +1,295 @@
+
+
+                                    
+									
+									
+                                        
+                                    const viewer = document.getElementById("modelblock");
+									let hotspotCounter = 0;
+									
+									
+									
+                                    
+
+									
+                                    function selected(){
+												// Add active class to the current button (highlight it)
+									
+									let hotspots = viewer.getElementsByClassName("hotspot");
+									
+
+									for (let i = 0; i < hotspots.length; i++) {
+									  hotspots[i].addEventListener("click", function() {
+									 let current = document.getElementsByClassName("selected");
+									  if (current.length > 0) { 
+										current[0].className = current[0].className.replace(" selected", "");
+										
+									  }
+									  this.className += " selected";
+									  
+									  });
+									}
+									
+										}
+
+										
+									
+									//modal window change
+
+									let topicCardValue = document.getElementById("topic-card-value"); //  modal window 
+									let sensorCardValue = document.getElementById("sensor-card-value");
+									let modalBox =document.querySelector('[class="modal-content"]');
+									let sensorCardUnit = document.getElementById("sensor-card-unit");
+
+									viewer.addEventListener('click',modalUpdate = (e) =>{
+										
+										let newData = e.target.closest("[data-sensornumber]");
+										
+										if (!newData) return;
+										
+										
+										topicCardValue.dataset.sensor=newData.dataset.sensornumber; // sensor and sensorval same data
+										sensorCardValue.dataset.sensorval=newData.dataset.sensornumber;
+										modalBox.dataset.modalCounterHotspot = newData.id; 
+									});	
+
+
+									// chek click target class
+									let param = {};
+									viewer.addEventListener('click',buttonClick = (e) =>{
+										
+										let newClick = e.target.className;
+										param.buttonControl = newClick
+										
+										
+										
+										 
+									});	
+									
+									
+									
+
+
+									
+										// Removes the selected hotspot
+
+										function removeHotspot(){
+										
+											let el =document.querySelector('[class="hotspot selected"]');
+											
+	
+											if (el == null){
+												alert("No hotspots to delete 2");
+											}else{
+												
+										el.remove();
+												
+									}; 
+										}
+										
+
+
+										// function for hotspot button 
+										
+
+									
+										//hotspot counter so we can keep track of how many we added on 1 because we already have hotspot-0 and hotspot-1 (set to 0 if you start with 0 hotspots)
+										
+									
+										function addHotspot(MouseEvent) {
+											
+										
+											let newsensorname = document.getElementById("hotspottext").value;
+											newsensorname = newsensorname == "" ? "no name" :  newsensorname;
+											/*
+											if (newsensorname == ""){
+												newsensorname ="no name";
+											}
+											*/
+											const newSensorUnit = document.getElementById("hotspotunit").value;
+											
+											
+											let inputtext =document.querySelector("option:checked").dataset.sensorvalue;  // test data flow for drop down list 
+
+											//new data input integration 
+											//let inputtext = document.querySelector(".sensor").dataset.sensorvalue;
+
+
+											
+
+
+											// if input = nothing then alert error if it isnt then add the hotspot
+
+											if (inputtext == undefined && param.buttonControl!="btn"  ){
+										//alert("Input sensors topic firs, to add on model.");
+										$("#ModalAlert").modal();
+									}else{
+										   
+									const viewer = document.querySelector('#modelblock');
+									const rect = viewer.getBoundingClientRect();
+									// coordinates calculating
+									const x = event.clientX - rect.left;
+									const y = event.clientY - rect.top;
+									const positionAndNormal = viewer.positionAndNormalFromPoint(x, y);
+									
+									// if the model is not clicked return the position in the console
+									if (positionAndNormal == null) {
+									console.log('no hit result: mouse = ', x, ', ', y);
+									return;
+									}
+									const {position, normal} = positionAndNormal;
+									
+									// create the hotspot
+									const hotspot = document.createElement('button');
+									hotspot.slot = `hotspot-${hotspotCounter ++}`;
+									hotspot.classList.add('hotspot');
+									hotspot.id = `hotspot-${hotspotCounter}`;
+									hotspot.dataset.toggle ="modal"						// modal window now sstatic , but foe update
+									hotspot.dataset.target ="#exampleModalCenter"
+
+									hotspot.dataset.sensornumber = viewer.querySelector(".sensor").dataset.sensorid;   // changing from sensor
+									hotspot.dataset.position = position.toString();
+									
+									if (normal != null) {
+									hotspot.dataset.normal = normal.toString();
+									}
+									viewer.appendChild(hotspot);
+									console.log('mouse = ', x, ', ', y, positionAndNormal);
+									
+									
+
+									let elementDataContainer = document.createElement("div");
+							
+									elementDataContainer.id= `elementdatacontainer-${hotspotCounter}`;
+									elementDataContainer.classList.add("elementdatacontainer");
+									elementDataContainer.dataset.sensorTopic = document.querySelector("#sensor-1").dataset.sensorTopic;
+									document.getElementById(`hotspot-${hotspotCounter}`).appendChild(elementDataContainer);
+									
+									let elementName = document.createElement("div");
+									elementName.classList.add('element-name');
+									
+									elementName.appendChild(document.createTextNode(newsensorname));
+									document.getElementById(`elementdatacontainer-${hotspotCounter}`).appendChild(elementName);
+
+
+									// adds the text with value to last hotspot
+									let element = document.createElement("div");
+									element.classList.add('annotation');
+									//element.dataset.sensorval=document.querySelector("option:checked").dataset.sensor;
+									let spanData = document.createElement("span");
+									// value element
+									spanData.dataset.sensorval=viewer.querySelector(".sensor").dataset.sensorid;
+									//spanData.innerHTML=document.querySelector("#sensor-1").dataset.sensorvalue;  // before it was with  span data 
+									spanData.id = `spandata-${hotspotCounter}`;
+									spanData.appendChild(document.createTextNode(inputtext))
+									// name of the unit
+									let spanUnit = document.createElement("span");
+									spanUnit.id=`spanunit-${hotspotCounter}`
+									spanUnit.innerHTML = " "+newSensorUnit;
+									//element.appendChild(document.createTextNode(inputtext));
+									element.appendChild(spanData);
+									element.appendChild(spanUnit);
+									document.getElementById(`elementdatacontainer-${hotspotCounter}`).appendChild(element);
+									
+									document.getElementById("hotspottext").value = "";
+									document.getElementById("hotspotunit").value="";
+
+
+									}
+									}
+									
+
+
+									
+									
+									
+										function removeHotspotlast(){
+										let el = document.getElementById(`hotspot-${hotspotCounter}`);
+										
+										if (el == null){
+											alert("No hotspots to delete");
+										}else{
+											hotspotCounter --;
+									el.remove()}; // Removes the last added hotspot
+									}
+
+									function myOpenClosemenu() {
+										let x = document.querySelector('[class="dim"]');
+										let buttom = document.getElementById("controls-button");
+									if (x.style.display === "none") {
+										x.style.display = "block";
+										buttom.style.display = "none";
+
+									} else {
+										x.style.display = "none";
+										buttom.style.display = "block";
+									}
+									}
+
+									// marker names update in modal window
+
+									function updateHotspot(){
+										
+										//document.querySelector('[data-modal-counter-hotspot]')
+										let newHotspotName=document.getElementById("hotspottextmodal").value
+										let newHotspotUnit=document.getElementById("hotspotunitchange").value
+										
+										
+										
+										
+										if (!newHotspotName&&!newHotspotUnit) return;
+										const hotspotNumbertoUpdate = document.querySelector('[data-modal-counter-hotspot]');
+										const hotspotId=hotspotNumbertoUpdate.dataset.modalCounterHotspot
+										
+										console.log(parseInt(hotspotId.match(/\d+/)));
+										let parrent = viewer.querySelector("#"+hotspotId);
+		
+										let child = parrent.querySelector(".element-name");
+										let unitEl = document.getElementById("spanunit-"+hotspotId.match(/\d+/));
+										if (newHotspotName){  // chek if empty, not to re write with empty
+											child.innerHTML=newHotspotName;// adding new name for marker
+										} else if (newHotspotUnit){
+											unitEl.innerHTML=" "+newHotspotUnit  // adding new Unit for marker
+
+										}
+										  
+										 
+
+										console.log(newHotspotName);
+										console.log(newHotspotUnit);
+										document.getElementById("hotspottextmodal").value ="";
+										newHotspotUnit=document.getElementById("hotspotunitchange").value="";
+										
+											
+									}
+
+									//camera position 
+
+									function cameraPosition(el){
+
+										console.log(el.value);
+										
+
+										switch (el.value){
+											case "left":
+											viewer.cameraOrbit ="270deg 80deg 30%";
+											break;
+											case "top":
+											viewer.cameraOrbit ="0deg 0deg 50%";
+											break;
+											case "front":
+											viewer.cameraOrbit ="0deg 80deg 30%";
+											break;
+											case "back":
+											viewer.cameraOrbit ="180deg 80deg 30%";
+											break;
+											case "right":
+											viewer.cameraOrbit ="90deg 80deg 30%";
+											break; 	 	
+										}
+
+									}
+									
+
+
+									
---
+++ assets/js/mqtt.js
@@ -0,0 +1,190 @@
+let host = 'sgrg.saint-gobain.com';
+let port = 443;
+let topic = 'VALUE/DE0161-SGR/TESTSENSORS/EF389C/TEMPERATURE';
+let username = 'websocket';
+let pw = 'ws-secret';
+let useTLS = true;
+let cleansession = true;
+let reconnectTimeout = 3000;
+let tempData = new Array();
+let mqtt;
+
+
+
+
+let cliendId = "mqtt_panel" + parseInt(Math.random() * 100);
+
+function MQTTconnect() {
+    if (typeof path == "undefined") {
+        path = '/mqtt';
+    }
+    
+    mqtt = new Paho.MQTT.Client(host, port, path, cliendId, 10);
+    
+    let options = {
+        timeout: 3,
+        useSSL: useTLS,
+        cleanSession: cleansession,
+        onSuccess: onConnect,
+		userName: username,
+		password: pw,
+        onFailure: function (message) {
+            $('#status').html("Connection failed: " + message.errorMessage + "Retrying...")
+                .attr('class', 'alert alert-danger');
+            setTimeout(MQTTconnect, reconnectTimeout);
+        }
+    };
+
+    mqtt.onConnectionLost = onConnectionLost;
+    mqtt.onMessageArrived = onMessageArrived;
+    console.log("Host: " + host + ", Port: " + port + ", Path: " + path + " TLS: " + useTLS + 'ClientId' + cliendId);
+    mqtt.connect(options);
+};
+
+function onConnect() {
+    $('#status').html('Connected to ' + host + ':' + port + path)
+        .attr('class', 'alert alert-success');
+    mqtt.subscribe(topic, { qos: 0 });
+    $('#topic').html(topic);
+
+    $("#connection-status").html("Connected").attr('class', 'alert alert-success');  // connection status in menu highlight
+
+
+};
+
+
+
+function onConnectionLost(response) {
+    setTimeout(MQTTconnect, reconnectTimeout);
+    $('#status').html("Connection lost. Reconnecting...")
+        .attr('class', 'alert alert-warning');
+    $("#connection-status").html("Connection lost").attr('class', 'alert alert-warning');   // disconnection status in menu highlight 
+};
+
+
+
+
+
+
+const topicArray = [];   
+topicArray.push(topic);   // array for tests 
+
+
+$(document).ready(function(){
+    let i =1; 
+    $("#hotspottopic").change(function(){
+        let userMessageTopic = document.getElementById("hotspottopic").value;
+        
+        if (topicArray.includes(userMessageTopic) ){
+            
+            i++;
+            $("#sensor-container").prepend(i);
+            
+
+            
+        }else {
+            console.log("not right name");
+        }
+
+
+      
+    });
+  });
+
+
+
+
+
+
+
+
+function onMessageArrived(message) {
+    let topic = message.destinationName;
+    let payload = message.payloadString;
+
+    
+    
+    
+    let userMessageTopic = document.getElementById("hotspottopic").value;
+    
+
+
+   
+
+
+   
+   
+    console.log("Topic: " + topic + ", Message payload: " + payload);
+    $('#message').html(topic + ', ' + payload);
+    
+	$('#mqtt-topic').text(topic + ':');
+	$('#mqtt-value-label').text(payload + ' °C');
+    
+	$('#mqtt-value-label').addClass('badge-default');
+    
+    $('#mqttdata').html('Temp is: ' +payload + ' °C');
+    $('#mqttdatadinamic').html('Temp is: ' +payload + ' °C');
+    
+    $('[data-sensor="sensor1"]').html(topic);
+    $('[data-sensor="sensor2"]').html(topic); /
+    $('[data-sensor="sensor4"]').html(topic);
+    $('[data-sensor="sensor5"]').html(topic);
+
+    $('[data-sensorval="sensor1"]').html(payload );
+    $('[data-sensorval="sensor2"]').html(payload );
+    $('[data-sensorval="sensor3"]').html(payload );
+    $('[data-sensorval="sensor4"]').html(payload );
+    $('[data-sensorval="sensor5"]').html(payload );
+	
+    /*
+    tempData.push({
+		"timestamp": Date().slice(16, 21),
+		"temperature": parseInt(payload)
+	});
+	if (tempData.length >= 10) {
+		tempData.shift()
+	}
+	drawChart(tempData);
+*/
+    
+ };
+/*
+function drawChart(data) {
+    let ctx = document.getElementById("chart").getContext("2d");
+
+
+    let temperatures = []
+    let timestamps = []
+
+    data.map((entry) => {
+        temperatures.push(entry.temperature);
+        timestamps.push(entry.timestamp);
+    });
+
+    let chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timestamps,
+            datasets: [{
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: temperatures
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            }
+        }
+    });
+}
+*/
+
+   
+
+
+$(document).ready(function () {
+  /* drawChart(tempData); */ 
+    MQTTconnect();
+});
+
